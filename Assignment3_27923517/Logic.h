ea6b1fd1b045542c858f8f09dcea1375a75c187d
Jonathan Nicholas <jonicrecis@gmail.com>
2017-10-09 23:39:04 +1100

    some class defining

--- Assignment3_27923517/Logic.h
+++ Assignment3_27923517/Logic.h
@@ -1,4 +1,14 @@
 #ifndef LOGIC_H
 #define LOGIC_H
+#include "Character.h"
+#include "Special.h"
+#include "Hero.h"
+#include <vector>
+class Logic{
+public:
 
+private:
+	std::vector<Character> InvolvedCharacters;
+
+}
 #endif //LOGIC_H
